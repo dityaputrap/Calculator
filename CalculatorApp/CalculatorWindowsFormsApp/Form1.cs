2020f1306d5cb689d6881af4698e2df03d55a5b7
dityaputrap <91526569+dityaputrap@users.noreply.github.com>
2021-12-21 14:20:27 +0700

    Add files via upload

---
+++ CalculatorApp/CalculatorWindowsFormsApp/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using CalculatorMachine;
+
+namespace CalculatorWindowsFormsApp
+{
+    public partial class Form1 : Form
+    { 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+              
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var a = int.Parse(txtNilaiA.Text);
+            var b = Convert.ToInt32(txtNilaiB.Text);
+            var cal = new Calculator();
+
+            lstHasil.Items.Clear();
+
+            lstHasil.Items.Add(string.Format("Hasil Penambahan : {0} + {1} = {2}", a, b, cal.Penambahan(a, b)));
+            lstHasil.Items.Add(string.Format("Hasil Pengurangan : {0} - {1} = {2}", a, b, cal.Pengurangan(a, b)));
+            lstHasil.Items.Add(string.Format("Hasil Perkalian : {0} * {1} = {2}", a, b, cal.Perkalian(a, b)));
+            lstHasil.Items.Add(string.Format("Hasil Pembagian : {0} / {1} = {2}", a, b, Calculator.Pembagian(a, b)));
+            lstHasil.Items.Add(string.Format("Hasil Pangkat : {0} ^ {1} = {2}", a, b, cal.Pangkat(a, b)));
+            lstHasil.Items.Add(string.Format("Hasil Modulo : {0} % {1} = {2}", a, b, Calculator.Modulo(a, b)));
+        }
+    }
+}
